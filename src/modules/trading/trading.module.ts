2059e555ad745230ab4f34e22b3db49adb8b38e5
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-03-17 19:53:55 +0100

    refactor

--- src/modules/trading/trading.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TradingController } from './trading.controller';
-import { TradingService } from './trading.service';
-
-@Module({
-  controllers: [TradingController],
-  providers: [TradingService]
-})
-export class TradingModule {}
